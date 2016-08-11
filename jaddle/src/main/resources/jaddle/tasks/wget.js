172efe3619884765faaaa8a0da097127a744b228
xafero <georg.berendt@uni-oldenburg.de>
2016-08-12 00:14:45 +0200

    Added wget like task

---
+++ jaddle/src/main/resources/jaddle/tasks/wget.js
@@ -0,0 +1,28 @@
+(function () {
+    var imports = new JavaImporter(java.io, java.net, org.apache.commons.io);
+
+    with (imports) {
+		var download = function (work, url) {
+			var file = new File(work, url.toURL().getFile());
+			if (file.exists() && file.canRead())
+				return file;
+			var input = url.toURL().openStream();
+			file.getParentFile().mkdirs();
+			var out = new FileOutputStream(file);
+			IOUtils.copy(input, out);
+			out.flush();
+			out.close();
+			input.close();
+			print("Downloaded: " + file)
+			return file;
+		};
+        return {
+            exec: function (work, args) {
+				for (var i = 0; i < args.length; i++) {
+					var url = args[i];
+					download(work, URI.create(url));
+				}
+            }
+        }
+    }
+});
