37643e2e79bea6a7bae3ae827099b9734975af05
xafero <georg.berendt@uni-oldenburg.de>
2016-08-11 02:45:27 +0200

    Added simple initial plugin structure

--- jaddle/src/main/resources/app/core/tasks.js
+++ jaddle/src/main/resources/app/core/tasks.js
@@ -1,13 +1,28 @@
 (function () {
-    this.tasks = ['kack1', 'kack2'];
-    return {
-        listTasks: function () {
-            for (var i = 0; i < tasks.length; i++)
-                print(tasks[i]);
-        },
-        runTasks: function (ids) {
-            for (var i = 0; i < ids.length; i++)
-                print(ids[i]);
+    this.tasks = {};
+
+    var imports = new JavaImporter(org.springframework.core.io.support, org.apache.commons.io);
+
+    with (imports) {
+        var resolver = new PathMatchingResourcePatternResolver();
+        var resources = resolver.getResources("classpath*:jaddle/tasks/**/*.js");
+
+        for (var i = 0; i < resources.length; i++) {
+            var resource = resources[i];
+            var url = resource.getURL();
+            var alias = FilenameUtils.getBaseName(url);
+            tasks[alias] = url;
         }
-    };
+
+        return {
+            listTasks: function () {
+                for (var task in tasks)
+                    print(" * " + task);
+            },
+            runTasks: function (ids) {
+                for (var i = 0; i < ids.length; i++)
+                    print(ids[i]);
+            }
+        };
+    }
 });
