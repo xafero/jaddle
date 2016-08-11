049faa51fd3b9eff625242a04daaec75e95ff168
xafero <georg.berendt@uni-oldenburg.de>
2016-08-11 06:23:47 +0200

    Added JSON formatting task

---
+++ jaddle/src/main/resources/jaddle/tasks/jsonFmt.js
@@ -0,0 +1,29 @@
+(function() {
+	var imports = new JavaImporter(java.io, com.google.gson);
+
+	with (imports) {
+		return {
+			exec: function(args) {
+				for (var i = 0; i < args.length; i++) {
+					var folder = new File(args[i]);
+					var parser = new JsonParser();
+					var json = (new GsonBuilder()).setPrettyPrinting().create();
+					var files = folder.listFiles();
+					for (var j = 0; j < files.length; j++) {
+						var file = files[j];						
+						var input = file;
+						var output = file;
+						var reader = new FileReader(input);
+						var tree = parser.parse(reader);
+						var writer = new FileWriter(output);
+						json.toJson(tree, writer);
+						writer.flush();
+						writer.close();
+						reader.close();
+						print("Reformatted: " + file)
+					}
+				}
+			}
+		}
+	}
+});
