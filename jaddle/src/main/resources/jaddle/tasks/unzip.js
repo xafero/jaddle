b74f85ad114efc6efa724e7104541a09d1f11fda
xafero <georg.berendt@uni-oldenburg.de>
2016-08-13 01:01:55 +0200

    Unzip task completed

--- jaddle/src/main/resources/jaddle/tasks/unzip.js
+++ jaddle/src/main/resources/jaddle/tasks/unzip.js
@@ -1,23 +1,75 @@
 (function() {
-	var imports = new JavaImporter(java.io, org.apache.commons.io);
+	var imports = new JavaImporter(java.io,
+			org.apache.commons.compress.archivers,
+			org.apache.commons.compress.archivers.tar,
+			org.apache.commons.compress.archivers.zip,
+			org.apache.commons.compress.compressors.gzip,
+			org.apache.commons.compress.compressors.xz,
+			org.apache.commons.compress.utils);
 
 	with (imports) {
-		var unzip = function(root, file) {
+		var determine = function(file) {
 			var name = file.getName().toLowerCase();
 			var kind = null;
 			if (name.endsWith(".zip"))
 				kind = "ZIP";
-			if (name.endsWith(".tgz") || name.endsWith(".tar.gz"))
+			else if (name.endsWith(".tgz") || name.endsWith(".tar.gz"))
 				kind = "TGZ";
-			if (name.endsWith(".txz") || name.endsWith(".tar.xz"))
+			else if (name.endsWith(".txz") || name.endsWith(".tar.xz"))
 				kind = "TXZ";
-			print(root + " / " + file + " / " + kind);
+			return kind;
+		};
+		var detectArk = function(kind, input) {
+			if (kind.startsWith("T"))
+				return new TarArchiveInputStream(input);
+			else
+				return new ZipArchiveInputStream(input);
+		};
+		var detect = function(kind, input) {
+			if (kind.equals("TXZ"))
+				return new XZCompressorInputStream(input);
+			else if (kind.equals("TGZ"))
+				return new GzipCompressorInputStream(input);
+			else
+				return input;
+		};
+		var unpack = function(root, file, overwrite) {
+			var kind = determine(file);
+			var input = new FileInputStream(file);
+			var pack = detect(kind, input);
+			var ark = detectArk(kind, pack);
+			var i = 0;
+			var entry;
+			while ((entry = ark.getNextEntry()) != null) {
+				var name = new File(root, entry.getName());
+				i++;
+				if (name.exists() && name.canRead()) {
+					if (i == 1 && !overwrite) {
+						print(" Skipped extraction, '" + name.getName()
+								+ "' exists!");
+						return;
+					}
+					continue;
+				}
+				if (entry.isDirectory()) {
+					name.mkdir();
+					continue;
+				}
+				var out = new FileOutputStream(name);
+				IOUtils.copy(ark, out);
+				print("Extracted: " + name);
+				out.flush();
+				out.close();
+			}
+			ark.close();
+			pack.close();
+			input.close();
 		};
 		return {
 			exec : function(work, args, env) {
 				for (var i = 0; i < args.length; i++) {
 					var path = args[i];
-					unzip(work, new File(path));
+					unpack(work, new File(path), false);
 				}
 			}
 		}
