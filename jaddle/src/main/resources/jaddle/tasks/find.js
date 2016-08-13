17f3680278938309fdd22eda40212872db3592f4
xafero <georg.berendt@uni-oldenburg.de>
2016-08-13 04:29:20 +0200

    Added find executable task

---
+++ jaddle/src/main/resources/jaddle/tasks/find.js
@@ -0,0 +1,28 @@
+(function() {
+	var imports = new JavaImporter(org.apache.commons.io);
+
+	with (imports) {
+		var find = function(work, term) {
+			var files = FileUtils.listFiles(work, null, true);
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				var long = file.getName();
+				var name = FilenameUtils.getBaseName(file);
+				var ext = FilenameUtils.getExtension(file);
+				if ((long.equalsIgnoreCase(term) || name.equalsIgnoreCase(term))
+						&& file.canExecute())
+					return file;
+			}
+			return null;
+		};
+		return {
+			exec : function(work, args, env) {
+				for (var i = 0; i < args.length; i++) {
+					var arg = args[i];
+					var res = find(work, arg);
+					print(arg + ' = ' + res);
+				}
+			}
+		}
+	}
+});
